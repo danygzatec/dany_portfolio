cb1decbb029922b6cd2ca0d8b2bbe08d51a5d0e5
danygzatec <A00829404@tec.mx>
2023-08-10 11:58:16 -0600

    backend with sanity

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,19 @@
+import about from "./about"
+import brands from "./brands"
+import contact from "./contact"
+import experience from "./experience"
+import skills from "./skills"
+import testimonials from "./testimonials"
+import work from "./work"
+import workExperience from "./workExperience"
+
+export const schemaTypes = [
+    about,
+    brands,
+    contact,
+    experience,
+    skills,
+    testimonials,
+    work,
+    workExperience
+]
